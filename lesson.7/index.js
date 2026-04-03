b3d90a7fabdc6eb53f9b4d2f70999d57526694e4
azizbekabdukarimov574-star <azizbekabdukarimov574@gmail.com>
2026-04-03 10:37:59 +0500

    git buyruqlari darsi

--- lesson.7/index.js
+++ lesson.7/index.js
@@ -1,1 +1,26 @@
 console.log("github")
+
+// git init
+// git remote add origin https://repository-manzili
+// git add .
+// git commit -m "comentariyaga yoziladi"
+// git push origin master
+// ================================
+
+// qoshimcha sorab qolsa
+
+//   git config --global user.email "you@example.com"
+//   git config --global user.name "Your Name"
+
+
+// ============================================================
+
+// doim yangi code yosak 3ta git buyrugini doim ishlatishimiz shart
+
+// git add .
+
+// git commit -m "komentariya yozing"
+
+// git add .
+// git commit -m "komentariya yozing"
+// git
